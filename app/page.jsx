2486354d695735373ee446ce041ce50b115dd5c4
Hamna Kanwal <hamnakanwal725@gmail.com>
2026-03-27 19:54:11 +0500

    updated

--- app/page.jsx
+++ app/page.jsx
@@ -1,35 +1,38 @@
 "use client";
+
+// Saare sections ab "Components" folder se directly aa rahe hain
 import HeroSlider from "./Components/Heroslider";
 import BrandSlider from "./Components/Brandslider";
 import ProductRangeSlider from "./Components/ProductRangeSlider";
-import Herosection from "./Herosection/page";
-import AmazingFlavour from "./AmazingFlavour/page";
-import ProductRange from"./ProductRange/page";
-import Crammed from "./Crammed/page";
+import Herosection from "./Components/Herosection";       // Ab direct file hai
+import AmazingFlavour from "./Components/AmazingFlavour"; // Ab direct file hai
+import ProductRange from "./Components/ProductRange";     // Ab direct file hai
+import Crammed from "./Components/Crammed";               // Ab direct file hai
 import CrammedSlider from "./Components/CrammedSlider";
 import Wowchow from "./Components/Wowchow";
-import Choice from "./Choices/page"
+import Choice from "./Components/Choices";                // Check karlein agar file name 'Choices' hai
 import CategorySlider from "./Components/CategorySlider";
 import BrandIntro from "./Components/BrandIntro";
 import SmoothSlider from "./Components/SmoothSlider";
 import Footer from "./Components/Footer";
+
 export default function Home() {
   return (
     <>
       <HeroSlider />
-        <BrandSlider/>
+      <BrandSlider />
       <Herosection />
-      <AmazingFlavour/>
-      <ProductRange/>
-      <ProductRangeSlider/>
-      <Crammed/>
-      <CrammedSlider/>
-      <Wowchow/>
-      <Choice/>
-<CategorySlider/>
-<BrandIntro/>
-<SmoothSlider/>
-<Footer/>
+      <AmazingFlavour />
+      <ProductRange />
+      <ProductRangeSlider />
+      <Crammed />
+      <CrammedSlider />
+      <Wowchow />
+      <Choice />
+      <CategorySlider />
+      <BrandIntro />
+      <SmoothSlider />
+      <Footer />
     </>
   );
 }
