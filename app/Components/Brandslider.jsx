23c0b417394eda71ba702f983a9ab3312da89847
Usman <usmanhadier03@gmail.com>
2026-06-08 14:25:47 +0500

    heading change

--- app/Components/Brandslider.jsx
+++ app/Components/Brandslider.jsx
@@ -19,8 +19,9 @@ const BrandsSlider = () => {
   ];
 
   return (
-    <section className="bg-[#4e1a51]">
-      <div className="max-w-7xl">
+    <section className="bg-[#4e1a51] w-full flex justify-center items-center py-4">
+      {/* mx-auto se slider screen ke center me rhega */}
+      <div className="w-full max-w-7xl mx-auto px-4">
         <Swiper
           modules={[Autoplay]}
           loop={true}
@@ -31,20 +32,16 @@ const BrandsSlider = () => {
             disableOnInteraction: false,
           }}
           breakpoints={{
+            500: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
             768: {
               slidesPerView: 3,
               spaceBetween: 40,
             },
             1024: {
-              slidesPerView: 4, // Slides kam karne se size mazeed bara dikhay ga
-              spaceBetween: 50,
-            },
-             768: {
-              slidesPerView: 3,
-              spaceBetween: 40,
-            },
-            500: {
-              slidesPerView: 4, // Slides kam karne se size mazeed bara dikhay ga
+              slidesPerView: 4, 
               spaceBetween: 50,
             },
           }}
@@ -53,13 +50,13 @@ const BrandsSlider = () => {
           {brands.map((brand, index) => (
             <SwiperSlide key={index}>
               <div className="flex justify-center items-center py-1">
-                <div className="relative w-[180px] h-[100px] md:w-[120px] md:h-[50px]"> 
+                <div className="relative w-[180px] h-[80px] md:w-[120px] md:h-[40px]"> 
                   <Image
                     src={brand.src}
                     alt={brand.alt}
                     fill
                     className="object-contain grayscale hover:grayscale-0 transition-all duration-300"
-                    sizes="(max-width: 768px) 180px, 220px"
+                    sizes="(max-width: 768px) 180px, 120px"
                   />
                 </div>
               </div>
