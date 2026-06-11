353ec68aaf6e3a4fdab7e71559a23df8edebfee6
Hamna Kanwal <hamnakanwal725@gmail.com>
2026-06-11 15:41:56 +0500

    issue resolved

--- app/categories/[CategorySlug]/[ProductSlug]/[sku]/page.jsx
+++ app/categories/[CategorySlug]/[ProductSlug]/[sku]/page.jsx
@@ -1,9 +1,9 @@
-import { getProductDetail, getRelatedProducts, getAllProducts } from "../../../../action";
+import { getProductDetail, getRelatedProducts,fetchAllProducts } from "../../../../action";
 import ProductDetail from "../../../../Components/ProductDetail";
 import { notFound } from "next/navigation";
 
 export async function generateStaticParams() {
-  const allProducts = await getAllProducts(); 
+  const allProducts = await fetchAllProducts(); 
   return allProducts.map((p) => ({
     CategorySlug: p.categorySlug, 
     ProductSlug: p.slug,
@@ -12,7 +12,6 @@ export async function generateStaticParams() {
 }
 
 export default async function Page({ params }) {
-  const { slug, sku } = await params;
   const { CategorySlug, ProductSlug, sku } = await params;
   
   const product = await getProductDetail(sku);
