9170b5f479e5d00640d8f6774743a82b4b201f3e
Hamna Kanwal <hamnakanwal725@gmail.com>
2026-06-18 17:10:30 +0500

    updated

--- app/Components/ProductRange.jsx
+++ app/Components/ProductRange.jsx
@@ -13,8 +13,8 @@ const FeaturedProducts = () => {
   const brandPurple = "#4e1a51";
 
   const products = [
-    { id: 1, src: "/featured.jpeg", alt: "Lychee Pulp", target: "coco-choo-coming-soon" },
-    { id: 2, src: "/cocochoo feature.jpeg", alt: "Coconut Water", target: "pulp-plus" },
+    { id: 1, src: "/cocochoo feature.jpeg", alt: "Lychee Pulp", target: "coco-choo-coming-soon" },
+    { id: 2, src: "/feature-banner-2.webp", alt: "Coconut Water", target: "pulp-plus" },
     { id: 3, src: "/feature-products-popping.webp", alt: "Guava Juice", target: "popping-candy" },
   ];
 
